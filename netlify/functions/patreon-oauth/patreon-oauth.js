cc64c6d6da17b1584dc8f5da733ee96a8242de47
Benjamin <benjamin.d.warnecke@gmail.com>
2025-03-08 21:27:29 +0100

    netlify commit because you forgot to commit

--- netlify/functions/patreon-oauth/patreon-oauth.js
+++ netlify/functions/patreon-oauth/patreon-oauth.js
@@ -39,67 +39,79 @@ exports.handler = async (event, context) => {
 
   const token_url = 'https://www.patreon.com/api/oauth2/token?';
 
-  const encryption_password = "pokemon";
+  const secret = process.env.NETLIFY_SECRET;
+  const encryption_password = secret;
 
   try {
     // Send a POST request to exchange the authorization code for an access token
     let response = null;
     if (code) {
-      response = await fetch(token_url + new URLSearchParams({
-        code,
-        grant_type: 'authorization_code',
-        client_id,
-        redirect_uri,
-      }).toString(), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
+      response = await fetch(
+        token_url +
+          new URLSearchParams({
+            code,
+            grant_type: 'authorization_code',
+            client_id,
+            redirect_uri,
+          }).toString(),
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      );
     } else if (refresh_token) {
-      response = await fetch(token_url + new URLSearchParams({
-        grant_type: 'refresh_token',
-        client_id,
-        refresh_token,
-        redirect_uri, // This may not be needed depending on the provider
-      }).toString(), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
+      response = await fetch(
+        token_url +
+          new URLSearchParams({
+            grant_type: 'refresh_token',
+            client_id,
+            refresh_token,
+            redirect_uri, // This may not be needed depending on the provider
+          }).toString(),
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      );
     }
 
     const token = await response.json();
 
     if (response.ok) {
-
-      const userFetchUrl = 'https://www.patreon.com/api/oauth2/v2/identity?' + new URLSearchParams({
-        "include": "memberships.currently_entitled_tiers,memberships.campaign",
-        "fields[user]": "full_name,vanity",
-        "fields[member]": "currently_entitled_amount_cents,lifetime_support_cents,patron_status,pledge_cadence"
-      }).toString()
+      const userFetchUrl =
+        'https://www.patreon.com/api/oauth2/v2/identity?' +
+        new URLSearchParams({
+          include: 'memberships.currently_entitled_tiers,memberships.campaign',
+          'fields[user]': 'full_name,vanity',
+          'fields[member]': 'currently_entitled_amount_cents,lifetime_support_cents,patron_status,pledge_cadence',
+        }).toString();
 
       const userDataResponse = await fetch(userFetchUrl, {
         method: 'GET',
         headers: {
-          'Authorization': `Bearer ${token.access_token}`,
-          'Content-Type': 'application/json'
-        }
-      })
+          Authorization: `Bearer ${token.access_token}`,
+          'Content-Type': 'application/json',
+        },
+      });
       if (!userDataResponse.ok) {
-        throw new Error('Failed to fetch user info from Patreon: ' + await userDataResponse.text());
+        throw new Error('Failed to fetch user info from Patreon: ' + (await userDataResponse.text()));
       }
       const userInfo = await userDataResponse.json();
 
-      const userName = userInfo.data.attributes.vanity ?? userInfo.data.attributes.full_name ?? "CouldNotFindName";
-      const memberData = userInfo.included.filter(something => something.type === "member");
-      const myMemberData = memberData.find(memberInfo => memberInfo?.relationships?.campaign?.data?.id === "12346885");
+      const userName = userInfo.data.attributes.vanity ?? userInfo.data.attributes.full_name ?? 'CouldNotFindName';
+      const memberData = userInfo.included.filter((something) => something.type === 'member');
+      const myMemberData = memberData.find(
+        (memberInfo) => memberInfo?.relationships?.campaign?.data?.id === '12346885'
+      );
       const filteredMembershipData = {
         userName,
-        supportsMe: myMemberData?.attributes?.patron_status === "active_patron" || userName === "BenisBoy16",
+        supportsMe: myMemberData?.attributes?.patron_status === 'active_patron' || userName === 'BenisBoy16',
         currently_entitled_tiers: myMemberData?.relationships?.currently_entitled_tiers,
-      }
+      };
 
       // Return the access token to the frontend
       return {
